41fb99d34b4dce799d44cf72c773bda62764062c
KOTARO SUTO <ng1a5ss43@gmail.com>
2024-06-23 15:47:30 -0700

    Run `swiftformat` plugin

--- Scripts/Bump.swift
+++ Scripts/Bump.swift
@@ -4,14 +4,16 @@ precondition(CommandLine.arguments.count == 3)
 
 let releaseConfigPath = "../Configs/Release.xcconfig"
 guard let content = FileManager.default.contents(atPath: releaseConfigPath),
-      let contentString = String(data: content, encoding: .utf8) else {
+      let contentString = String(data: content, encoding: .utf8)
+else {
     fatalError()
 }
 
 guard let replaced = contentString
     .replacing(#/CI_MARKETING_VERSION/#, with: CommandLine.arguments[1])
     .replacing(#/CI_CURRENT_PROJECT_VERSION/#, with: CommandLine.arguments[2])
-    .data(using: .utf8) else {
+    .data(using: .utf8)
+else {
     fatalError()
 }
 
